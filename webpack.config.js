6c0a98c63b9a6c804c3c0384f42e7976000ef7dc
yaswanth-pula <yaswanth.pula@gmail.com>
2021-06-02 11:14:26 +0530

    Intial Webpack Config for react App

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.png|jpg|svg|jpeg$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./public/index.html"),
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    contentBase: path.resolve(__dirname, "./public/index.html"),
+  },
+};
